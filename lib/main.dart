ba892261f38bc543c28d6662d0a00a68b30f1c17
MiltonQuirino <miltonquirino.br@gmail.com>
2022-03-13 11:45:46 -0300

    add and remove itens

--- lib/main.dart
+++ lib/main.dart
@@ -34,26 +34,71 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var newTaskCtrl = TextEditingController();
+
+  void add() {
+    if (newTaskCtrl.text.isEmpty) return;
+
+    setState(() {
+      widget.items.add(Item(title: newTaskCtrl.text, done: false));
+      newTaskCtrl.clear();
+    });
+  }
+
+  void remove(int index) {
+    widget.items.removeAt(index);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Todo List"),
+        title: TextFormField(
+          keyboardType: TextInputType.text,
+          controller: newTaskCtrl,
+          decoration: InputDecoration(
+            labelText: "Nova Tarefa",
+            labelStyle: TextStyle(color: Colors.white),
+          ),
+          // validator: (value) {
+          //   if (value.isEmpty) {
+          //     return "*";
+          //   }
+          // },
+        ),
       ),
       body: ListView.builder(
           itemCount: widget.items.length,
           itemBuilder: (BuildContext context, int index) {
             final item = widget.items[index];
-            return CheckboxListTile(
-              title: Text(item.title),
+            return Dismissible(
+              child: CheckboxListTile(
+                title: Text(item.title),
+                value: item.done,
+                onChanged: (value) {
+                  setState(() {
+                    item.done = value!;
+                  });
+
+                  print(value);
+                },
+              ),
               key: Key(item.title),
-              value: item.done,
-              onChanged: (value) {
-                // item.done = value;
-                print(value);
+              background: Container(
+                color: Colors.red.withOpacity(0.2),
+              ),
+              onDismissed: (direction) {
+                remove;
+                print(direction);
               },
             );
+            // return
           }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: add,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.pink,
+      ),
     );
   }
 }
